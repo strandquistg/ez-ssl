4ed92b689b72d1c8f9d38d3e967c6b51d4294d11
strandquistg <gsquist@cs.washington.edu>
2020-11-23 22:55:12 -0800

    code for wrist epochs

--- pretasks/make_ecog_data.py
+++ pretasks/make_ecog_data.py
@@ -6,13 +6,14 @@ import cv2
 from sklearn.model_selection import KFold
 from signal_transforms import *
 from sklearn.model_selection import train_test_split
+import xarray as xr
 
 #apply signal transformations to epoched ecog data,
 #return in train/test format to use in HTNet
 def speech_signal_transform(sbj, lp):
     sigs, labs = [], []
     transform_task = [0, 1, 2, 3, 4] #, 5, 6
-    noise_amount, scaling_factor, permutation_pieces = 10, 15, 9  #
+    noise_amount, scaling_factor, permutation_pieces = 20, 15, 9  #
     f_load = natsort.natsorted(glob.glob(lp+'/*_epo.fif'))
     print("in signal transform func", lp+sbj,"\nwith files",f_load)
     for i, f in enumerate(f_load):
@@ -28,4 +29,36 @@ def speech_signal_transform(sbj, lp):
         total_sigs = np.vstack (( signal, noised_signal, scaled_signal, negated_signal, flipped_signal))
         total_labs = np.repeat(transform_task, signal.shape[0])
     X_train, X_test, y_train, y_test = train_test_split(total_sigs, total_labs, stratify=total_labs, test_size=0.33)
+    return np.expand_dims(X_train,1), y_train, np.expand_dims(X_test,1), y_test
+
+
+
+#
+def wrist_signal_transform(sbj, lp, n_chans_all=64, tlim=[-3,3], event_types=['rest','move']):
+    transform_task = [0, 1, 2, 3, 4] #, 5, 6
+    noise_amount, scaling_factor, permutation_pieces = 10, 15, 9  #
+    ep_data_in = xr.open_dataset(lp+sbj+'_ecog_data.nc')
+    ep_times = np.asarray(ep_data_in.time)
+    time_inds = np.nonzero(np.logical_and(ep_times>=tlim[0],ep_times<=tlim[1]))[0]
+    days_all_in = np.asarray(ep_data_in.events)
+    days_train = np.unique(days_all_in)[:-1]
+    #Compute indices of days_train in xarray dataset
+    days_train_inds = []
+    for day_tmp in list(days_train):
+        days_train_inds.extend(np.nonzero(days_all_in==day_tmp)[0])
+    #Extract data
+    dat_train = ep_data_in[dict(events=days_train_inds,channels=slice(0,n_chans_all),
+                                time=time_inds)].to_array().values.squeeze()
+
+    #Apply signal transformations
+    noised_signal       = add_noise(dat_train, noise_amount = noise_amount) #round(np.random.uniform(0.005,0.05),2)) # 0.005 - 0.05
+    scaled_signal       = scaled(dat_train, factor = scaling_factor) #round(np.random.uniform(0.2,2),2)) # 0.2 - 2
+    negated_signal      = negate(dat_train)
+    flipped_signal      = hor_filp(dat_train)
+    #permuted_signal     = permute(signal, pieces = permutation_pieces) # 2-20
+    # time_warped_signal = time_warp(dat_train, 500, pieces = time_warping_pieces, stretch_factor = time_warping_stretch_factor, squeeze_factor = time_warping_squeeze_factor)
+    train_sigs = np.vstack (( dat_train, noised_signal, scaled_signal, negated_signal, flipped_signal))
+    train_labs = np.repeat(transform_task, dat_train.shape[0])
+
+    X_train, X_test, y_train, y_test = train_test_split(train_sigs, train_labs, stratify=train_labs, test_size=0.33)
     return np.expand_dims(X_train,1), y_train, np.expand_dims(X_test,1), y_test
